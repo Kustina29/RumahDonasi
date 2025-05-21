d12483495ccb9d2a091ec9acda2646872b5abb6a
Lutfi Alfarizi <lutfial1908@gmail.com>
2025-05-22 06:53:51 +0800

    Add files via upload

---
+++ Project Web/js/script.js
@@ -0,0 +1,15 @@
+// Toogle class active
+const navbarNav = document.querySelector('.navbar-nav');
+
+document.querySelector('#menu').onclick = () => {
+    navbarNav.classList.toggle('active')
+}
+
+// Hide Nav
+const menu = document.querySelector('#menu');
+
+document.addEventListener('click', function(e) {
+    if(!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active')
+    }
+})
